b29622edc478b0cdde01de4cd8dfa9da435784e7
nonlin-lin-chaos-order-etc-etal <hypnosis.i2p@gmail.com>
2023-08-12 01:12:23 +0800

    Add tray icon & closer tray menu item

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # elementwrapper
 A lightweight Electron wrapper for https://app.element.io/
+
+To run under Linux, use: `dbus-launch yarn start`
--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,14 +1,37 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, Tray } = require('electron');
+
+let win = null;
+let tray = null;
+const ICON="./icon.jpg";
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    icon: ICON
   });
-
+  
   win.loadURL('https://app.element.io/');
+}
+
+const toggle_show_hide = () => {
+  if (win.isVisible()) win.hide();
+  else win.show()
+}
+
+const quit = () => {
+  win.destroy(); // see also: https://www.electronjs.org/docs/latest/api/browser-window#winclose
+  tray.destroy();
 }
 
 app.whenReady().then(() => {
   createWindow();
+  tray = new Tray(ICON)
+  tray.on('click', toggle_show_hide);
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Show/Hide Element', type: 'normal', click: toggle_show_hide },
+    { label: 'Quit', type: 'normal', click: quit }
+  ])
+  tray.setToolTip('Element')
+  tray.setContextMenu(contextMenu)
 })
